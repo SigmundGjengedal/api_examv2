49439dd704300c6eb453d0721ed8188d985a03bc
SigmundGjengedal <sigmund.gjengedal@gmail.com>
2022-05-04 00:21:40 +0200

    removed endSession

--- client/util/applicationApiContext.jsx
+++ client/util/applicationApiContext.jsx
@@ -12,12 +12,6 @@ export const ApplicationApiContext = React.createContext({
     async registerLogin(provider, login) {
         return await postJSON(`/api/login/${provider}`, login);
     },
-    async endSession() {
-        const res = await fetch("/api/login", { method: "DELETE" });
-        if (!res.ok) {
-            throw new Error(`Failed to post ${res.status}: ${res.statusText}`);
-        }
-    },
     async createMovie(movie) {
         fetch("/api/movies/new", {
             method: "post",
